cdea4b878f3de7431b79d63f0dd55ca2c9618c41
Jie Zhang <j.zhang@student.tue.nl>
2022-10-05 16:05:55 +0200

    upload simulation model

---
+++ delta_drv90l_support/readme.md
@@ -0,0 +1,4 @@
+## **delta_drv90l_support**
+
+Show the URDF in RViz:
+*  `roslaunch delta_drv90l_support test_drv90l.launch`
---
+++ load_drv90l.py
@@ -0,0 +1,26 @@
+
+import pybullet as p
+from time import sleep
+import pybullet_data
+
+
+physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0, 0, -10)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("./delta_drv90l_support/urdf/drv90l.urdf",useFixedBase=True)
+
+
+useRealTimeSimulation = 0
+
+if (useRealTimeSimulation):
+  p.setRealTimeSimulation(1)
+
+while 1:
+  if (useRealTimeSimulation):
+    p.setGravity(0, 0, -10)
+    sleep(1000)  # Time in seconds.
+  else:
+    p.stepSimulation()
