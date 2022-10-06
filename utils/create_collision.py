719762f120a1928f8c470cbc1596043aa85c0b9a
Jie Zhang <j.zhang@student.tue.nl>
2022-10-06 16:31:29 +0200

    create collision for objects

---
+++ utils/create_collision.py
@@ -0,0 +1,14 @@
+import os
+import pybullet as p
+
+
+path = './objects_model/objs/meshes'
+
+p.connect(p.DIRECT)
+files = os.listdir(path)
+for file in files:
+    print('processing ...', file)
+    name_in = os.path.join(path, file)
+    name_out = os.path.join(path, file.replace('.obj', '_col.obj'))
+    name_log = "collision_log.txt"
+    p.vhacd(name_in, name_out, name_log)
